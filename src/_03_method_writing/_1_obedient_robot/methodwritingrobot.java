c1aa3df31c1193d46227fd6215458efca419a23b
root <root@f51d5bacf5f9>
2021-01-20 00:58:51 +0000

    I practiced writing methods!

--- src/_03_method_writing/_1_obedient_robot/methodwritingrobot.java
+++ src/_03_method_writing/_1_obedient_robot/methodwritingrobot.java
@@ -1,5 +1,9 @@
 package _03_method_writing._1_obedient_robot;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class methodwritingrobot {
@@ -8,7 +12,42 @@ public class methodwritingrobot {
 		// TODO Auto-generated method stub
 		rob.penDown();
 		rob.setSpeed(2);
-		drawSquare();
+		String color = JOptionPane.showInputDialog("What color would you like the robot's shape he will draw to be?");
+		if(color.equalsIgnoreCase("Red")) {
+		rob.setPenColor(Color.red);  //rob.setPenColor(255, 0, 0);
+		} else if(color.equalsIgnoreCase("Orange")) {
+			rob.setPenColor(Color.orange);
+		} else if(color.equalsIgnoreCase("Yellow")) {
+			rob.setPenColor(Color.yellow);
+		} else if(color.equalsIgnoreCase("Green")) {
+			rob.setPenColor(Color.green);
+		} else if(color.equalsIgnoreCase("Blue")) {
+			rob.setPenColor(Color.blue);
+		} else if(color.equalsIgnoreCase("Pink")) {
+			rob.setPenColor(Color.pink);
+		} else if(color.equalsIgnoreCase("White")) {
+			rob.setPenColor(Color.white);
+		} else if(color.equalsIgnoreCase("Black")) {
+			rob.setPenColor(Color.black);
+		} else if(color.equalsIgnoreCase("Cyan")) {
+			rob.setPenColor(Color.cyan);
+		} else if(color.equalsIgnoreCase("Magenta")) {
+			rob.setPenColor(Color.magenta);
+		} else if(color.equalsIgnoreCase("Gray")) {
+			rob.setPenColor(Color.gray);
+		} else {
+			JOptionPane.showMessageDialog(null, "Color not available!");
+		}
+		String shape = JOptionPane.showInputDialog("What shape do you would you like the robot to draw?");
+		if(shape.equalsIgnoreCase("circle")) {
+		drawCircle();
+		} else if(shape.equalsIgnoreCase("triangle")) {
+			drawTriangle();
+			}else if(shape.equalsIgnoreCase("square")) {
+				drawSquare();
+			} else {
+				JOptionPane.showMessageDialog(null, "Shape not available!");
+			}
 	}
 
 	static Robot rob = new Robot();
@@ -18,6 +57,18 @@ public class methodwritingrobot {
 		for (int sides = 0; sides < 4; sides++) {
 			rob.move(100);
 			rob.turn(90);
+		}
+	}
+	static void drawTriangle() {
+		for (int tri = 0; tri < 3; tri++) {
+			rob.move(133);
+			rob.turn(120);
+		}
+	}
+	static void drawCircle() {
+		for(int circle = 0; circle < 360; circle++) {
+			rob.move(1);
+			rob.turn(1);
 		}
 	}
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,23 +5,26 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String pet = JOptionPane.showInputDialog("What kind of pet do you want/have?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Cuddle with your pet", "Feed your pet", "Take your pet out on a walk" }, null);
 			// 5. Use user input to call the appropriate method created in step 4.
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
+	}
+	private static void showOptionDialog(Object object, String string, String string2) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	// 4. Create methods to handle each of your user selections.
